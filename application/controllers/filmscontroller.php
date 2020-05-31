2e08ba713c552b7ee0d25d124da08045a2d078a9
Lam Nguyen <lam@impl.vn>
2020-05-31 17:31:42 +0700

    make index up and running

--- application/controllers/filmscontroller.php
+++ application/controllers/filmscontroller.php
@@ -6,14 +6,46 @@
  * and open the template in the editor.
  */
 
+//class FilmObject {
+//    public $id;
+//    public $name;
+//    public $description;
+//    
+//    public function __construct($array) {
+//        $this->id = $array['id'];
+//        $this->name = $array['name'];
+//        $this->description = $array['description'];
+//    }
+//}
+
 class FilmsController extends BaseController {
     function beforeAction() {
         
     }
     
+    function view() {
+        
+    }
+    
     function index() {
-        $this->set('title', 'Index');
-        $this->set('film', 'Titanic');
+//        $this->Category->orderBy('name', 'ASC');
+//        $this->Category->showHasOne();
+//        $this->Category->showHasMany();
+//        $this->Category->where('parent_id', '0');
+//        $this->Film->orderBy('name', 'ASC');
+//        $this->Film->where('id', '1');
+        $films = $this->Film->search();
+//        $this->set('categories', $categories);
+//        dd($films);
+        $this->set('films', $films);
+    }
+    
+    function create() {
+        
+    }
+    
+    function store() {
+        
     }
     
     function afterAction() {
--- application/views/films/create.php
+++ application/views/films/create.php
@@ -6,6 +6,3 @@
  * and open the template in the editor.
  */
 
-class Film extends BaseModel {
-    
-}
