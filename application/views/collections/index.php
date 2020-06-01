6089c1519140b40725c55c6e8202e1a698f65766
tuananh <tuananh2431998@gmail.com>
2020-06-01 16:39:05 +0700

    index view

--- application/views/collections/index.php
+++ application/views/collections/index.php
@@ -1,4 +1,4 @@
-<div class="content bg-body pt-4">
+<div class="content bg-body pt-4 collection-body">
   <div class="container">
     <div class="text-center">
       Collect, curate, and share. Lists are the perfect way to group films.
@@ -11,13 +11,28 @@
     </div>
     <div class="content-body d-flex mt-4 horizontal-between">
       <div class="collection-card">
-
-      </div>
-      <div class="collection-card">
-
-      </div>
-      <div class="collection-card">
-        
+        <div class="film-stack w-100">
+            <div class="stack-item z-5"></div>
+            <div class="stack-item z-4"></div>
+            <div class="stack-item z-3"></div>
+            <div class="stack-item z-2"></div>
+            <div class="stack-item z-1"></div>
+        </div>
+        <div class="mt-2 mb-1 d-flex horizontal-between w-100">
+          <div class="fw-bold font-size-large"><a href="#">Collection name</a></div>
+          <div>20 films</div>
+        </div>
+        <div class="d-flex horizontal-between">
+          <div>
+            <a class="avatar" href="#">
+              <img src="img/avatar.jpeg" width="16" height="16">
+            </a>
+            <span class="fw-bold font-size-medium">Tuan Anh</span>
+          </div>
+          <div>
+            <div>&#10084; 1000 &#128172; 100</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
