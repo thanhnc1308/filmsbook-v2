0b1fc96bcaca8779dc0256c05e8a1adccf39bce3
huynv5 <huynv5@topica.edu.vn>
2020-06-02 10:38:04 +0700

    add login

---
+++ application/views/login/index.php
@@ -0,0 +1,22 @@
+<!-- content -->
+<div class="content bg-body pt-4">
+    <div class="login-box w-25 mx-auto pt-3 pb-3 pl-3 pr-3 rounded w-fit bg-login">
+        <div class="login-title text-center">
+            <h2>Sign in</h2>
+        </div>
+        <div class="login-form">
+            <form class="d-flex flex-column mb-3 border-bottom" action="" method="post">
+                <input class="pl-2 pr-2 pt-2 pb-2 mb-2 rounded" type="text" placeholder="Email address" name="email" id="email">
+                <input class="pl-2 pr-2 pt-2 pb-2 mb-2 rounded" type="password" placeholder="Password" name="password" id="password">
+                <input class="pl-2 pr-2 pt-2 pb-2 rounded bg-success" type="submit" value="Sign in">
+            </form>
+        </div>
+        <div class="divider pt-2 border border-bottom-0 border-left-0 border-right-0"></div>
+        <div class="register">
+            <form class="d-flex flex-column" action="" method="post">
+                <input class="pl-2 pr-2 pt-2 pb-2 rounded bg-danger" type="submit" value="Sign up New Account">
+            </form>
+        </div>
+    </div>
+</div>
+<!-- end content -->
