e6c82d8aa13659733da6b918348194a9514fc689
Lam Nguyen <lam@impl.vn>
2020-06-07 05:00:15 +0700

    add Countries

---
+++ application/controllers/countriescontroller.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CountriesController
+ *
+ * @author lamnt
+ */
+class CountriesController extends BaseController {
+    function beforeAction() {
+        
+    }
+    
+    function index() {
+        $countries = $this->Country->search();
+        $this->set('countries', $countries);
+    }
+    
+    function create() {
+        
+    }
+    
+    function store() {
+        if(isset($_POST['moviedb_id']) && isset($_POST['name'])) {
+            $moviedb_id = $_POST['moviedb_id'];
+            $name = $_POST['name'];
+            
+            $country = new Country();
+            $country->moviedb_id = $moviedb_id;
+            $country->name = $name;
+            $country->save();
+        }
+    }
+    
+    function afterAction() {
+        
+    }
+}
