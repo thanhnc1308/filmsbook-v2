86b0e4ba6dc7589e57f0b909ad2d19d985967be6
Lam Nguyen <lam@impl.vn>
2020-06-07 13:44:07 +0700

    finish filmscontroller

--- application/controllers/filmscontroller.php
+++ application/controllers/filmscontroller.php
@@ -18,13 +18,27 @@ class FilmsController extends BaseController {
     }
     
     function store() {
-        if(isset($_POST['name']) && isset($_POST['description'])) {
-            $name = $_POST['name'];
-            $description = $_POST['name'];
-            
+        if (isset($_POST['title']) 
+            && isset($_POST['overview'])
+            && isset($_POST['release_date'])
+            && isset($_POST['popularity'])
+            && isset($_POST['runtime'])
+        ) {
             $film = new Film();
-            $film->name = $name;
-            $film->description = $description;
+            $film->title = $_POST['title'];
+            $film->overview = $_POST['overview'];
+            $film->release_date = $_POST['release_date'];
+            $film->popularity = $_POST['popularity'];
+            $film->runtime = $_POST['runtime'];
+            
+            $film->moviedb_id = $_POST['moviedb_id'];
+            $film->budget = $_POST['budget'];
+            $film->original_language = $_POST['original_language'];
+            $film->poster_path = $_POST['poster_path'];
+            $film->revenue = $_POST['revenue'];
+            $film->vote_average = $_POST['vote_average'];
+            $film->vote_count = $_POST['vote_count'];
+            
             $film->save();
             
             $this->set('film', $film);
--- application/views/films/index.php
+++ application/views/films/index.php
@@ -2,13 +2,22 @@
     <div class="container">
         <div class="content-body d-flex mt-4">
             <?php
-//                var_dump($films);
+//                $category['Category']['name']
                 foreach($films as $film) {
-//                    <?php echo $html->link($category['Category']['name'], 'categories/view/' . $category['Category']['id'] . '/' . $category['Category']['name'])
-                    $name = $film['Film']['name'];
-                    $description = $film['Film']['description'];
-                    echo "<h1>Name: $name</h1>";
-                    echo "<p>Description: $description</p>";
+                    echo "<h1>Title: " . $film['Film']['title'] . "</h1>";
+                    echo "Overview: " . $film['Film']['overview'];
+                    echo "Release Date: " . $film['Film']['release_date'];
+                    echo "Popularity: " . $film['Film']['popularity'];
+                    echo "Runtime: " . $film['Film']['runtime'];
+                    
+                    echo "MovieDB's ID" . $film['Film']['moviedb_id'];
+                    echo "Budget: " . $film['Film']['budget'];
+                    echo "Original Language: " . $film['Film']['original_language'];
+                    echo "Poster Path: " . $film['Film']['poster_path'];
+                    echo "Revenue: " . $film['Film']['revenue'];
+                    echo "Vote Average: " . $film['Film']['vote_average'];
+                    echo "Vote Count: " . $film['Film']['vote_count'];
+                    
                     echo "<br>";
                     echo "<hr>";
                 }
