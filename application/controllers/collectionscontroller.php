8d7abe0af5c94815c15be03511a29df4a615f09e
tuananh <tuananh2431998@gmail.com>
2020-06-09 15:46:38 +0700

    index collections

--- application/controllers/collectionscontroller.php
+++ application/controllers/collectionscontroller.php
@@ -7,11 +7,17 @@ class CollectionsController extends BaseController{
   
   function index($user_id=null){
     $user_id = 2;
-    $collections = $this->Collection->custom(
+    $collections = [];
+    $collections_films = $this->Collection->custom(
     "SELECT collections.*, films.avatar FROM collections 
       INNER JOIN collections_films ON collections.id = collections_films.collection_id
       INNER JOIN films ON collections_films.film_id = films.id
       WHERE user_id = $user_id");
+    foreach($collections_films as $collection_film){
+      $collection_id = $collection_film['Collection']['id'];
+      $collections[$collection_id]['Collection'] = $collection_film['Collection'];
+      $collections[$collection_id]['Film'][] = $collection_film['Film']; 
+    }
     $this->set('collections', $collections);
   }
 
