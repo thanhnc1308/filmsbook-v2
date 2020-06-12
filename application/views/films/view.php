bf4d9a3ab6f97a604dcaa02568b467d540cc4ef9
Lam Nguyen <lam@impl.vn>
2020-06-12 14:50:14 +0700

    add review directly under films view

--- application/views/films/view.php
+++ application/views/films/view.php
@@ -7,6 +7,7 @@
     <div class="container">
         <div class="content-body mt-4">
             <?php
+                $film_id = $film['Film']['id'];
                 echo "<h1>Title: " . $film['Film']['title'] . "</h1>";
                 $poster_path = $film['Film']['avatar'];
                 echo "<img src=\"$poster_path\" width=\"154\">";
@@ -59,14 +60,23 @@
                     echo " as " . $actor['actors_films']['character_name'] . "<br>";
                 }
                 
+                $trailer = "http://www.youtube.com/embed/" . $film['Film']['trailer'];
+                echo "<br>Trailer:<br>";
+                echo "<iframe src=\"$trailer\" width=\"420\" height=\"270\" frameborder=\"0\" allowfullscreen></iframe>";
+                
                 echo "<br>Reviews:<br>";
                 foreach($reviews as $review) {
                     echo $review['Review']['username'] . " - " . $review['Review']['content'];
                 }
                 
-                $trailer = "http://www.youtube.com/embed/" . $film['Film']['trailer'];
-                echo "<br>Trailer:<br>";
-                echo "<iframe src=\"$trailer\" width=\"420\" height=\"270\" frameborder=\"0\" allowfullscreen></iframe>";
+                // add review
+                echo "<h2>Add your own review</h1>";
+                echo "<div>";
+                echo "<form method=\"post\" action=\"" . $html->getHref('reviews/store/' . $film_id) . "\">";
+                echo "<textarea name=\"content\" rows=\"4\" cols=\"50\"></textarea>";
+                echo "<input type=\"submit\" value=\"Add review\">";
+                echo "</form>";
+                echo "</div>";
             ?>
         </div>
     </div>
