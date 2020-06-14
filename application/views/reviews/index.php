ef2b4917381ad1d7ed0394d0e062e6e4000d5a2f
Lam Nguyen <lam@impl.vn>
2020-06-14 18:30:28 +0700

    view all reviews

--- application/views/reviews/index.php
+++ application/views/reviews/index.php
@@ -7,7 +7,16 @@
     
     <div class="container">
         <div class="content-body d-flex mt-4">
-            
+            <?php
+            foreach($reviews as $review) {
+                $id = $review['Review']['id'];
+                $author = $review['User']['username'];
+                $avatar = $review['Film']['avatar'];
+                
+                $review_thumbnail = new ReviewThumbnail($id, $author, $avatar);
+                $review_thumbnail->render($html);
+            }
+            ?>
         </div>
     </div>
     <!-- end tab content -->
